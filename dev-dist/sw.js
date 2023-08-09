41008b9fa7c78eec64fd83fba8a6fc90f83f6bbe
David <davidostos2@gmail.com>
2023-08-09 13:48:59 -0400

    [UPDATE] producto

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qgdei2ronn8"
+    "revision": "0.ijbb9t1j2mg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
