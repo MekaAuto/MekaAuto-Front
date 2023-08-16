0ee1cc4368857f55a653eb84d53ba5dba20438af
David <davidostos2@gmail.com>
2023-08-16 12:36:56 -0400

    [ADD] agregando espacio de status bar

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,7 @@ body {
   box-sizing: content-box;
   max-width: 100%;
   min-height: 100vh;
+  padding: env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left);
 }
 
 </style>
---
+++ src/views/components/StatusBar.vue
@@ -0,0 +1,17 @@
+<!-- <template></template>
+
+<script setup lang="ts">
+import { StatusBar, type StatusBarInfo, type BackgroundColorOptions } from '@capacitor/status-bar';
+/* import { ref } from 'vue'; */
+
+const props = defineProps<{
+    info: StatusBarInfo,
+    keys: string[],
+    backgroundColor: BackgroundColorOptions
+}>();
+
+const status = StatusBar
+
+</script>
+
+<style scoped lang="scss"></style> -->
