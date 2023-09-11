6b0d3f5fb996eac6103b778a624a01af112f8e84
David Ostos <davidostos2@gmail.com>
2023-09-11 10:25:56 -0400

    agregando cmabios en productHome

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rcq4vt8d6h8"
+    "revision": "0.6gel8d7fg68"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/views/products/productHome.vue
+++ src/views/products/productHome.vue
@@ -1,59 +1,54 @@
 <script setup lang="ts">
-import { onBeforeMount, onMounted, onUnmounted, ref } from 'vue';
+import { ref } from 'vue';
+import Products from '../container/products.vue';
+import TitleProducts from '../container/titleProducts.vue';
 
 const uri = import.meta.env.VITE_BACK_URL + '/post';
-
 const products: any = ref([]);
+const most_seen = ref([]);
 
-const most_seen = ref([]);/* 
-const advertisingSlider = ref([]);
-const new_products = ref([]);
-const top_sales = ref([]);
-const top_sales2 = ref([]);
- */
-const { title, title2 } = {
-  title: 'Nombre',
-  title2: 'Nombre 2'
-};
-
-fetch(uri, {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json'
-  }
-})
+fetch(uri)
   .then((res) => res.json())
   .then((data) => {
     products.value = data;
     most_seen.value = products.value.most_seen.data;
-/*     advertisingSlider.value = data.advertisingSlider;     new_products.value = products.value.new_products;
-    top_sales.value = products.value.top_sales;
-    top_sales2.value = products.value.top_sales2;*/
-    for (const product of most_seen.value){ 
-      console.log(product)
-    }
-    console.log(most_seen.value);
   });
 
 </script>
 
 <template>
-  <div class="container mx-auto">
-    <TitleProducts :title="title ?? 'nombre'" :title2="title2 ?? 'nombre 2'" />
+  <div class="container mx-auto"
+    v-for="(post, index) in products"
+    :key="post.id">
+    <TitleProducts  :title="index.toString()" />
     <div class="contenerdor">
       <div class="contenedor_scroll px-1">
         <Products
-          v-for="product in most_seen"
+          v-for="product in post"
           :key="product.id"
           :title="product.title"
           :images="product.img"
-          :condicion="product.excerpt"
+          :condicion="product.price"
           :descuento="product.price"
         ></Products> 
       </div>
     </div>
   </div>
+
+
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.contenerdor {
+  overflow: scroll;
+  scroll-snap-type: x mandatory;
+  box-sizing: border-box;
+}
+.contenerdor::-webkit-scrollbar {
+  display: none;
+}
+.contenedor_scroll {
+  display: flex;
+  padding-bottom: 40px;
+  position: relative;
+}</style>
